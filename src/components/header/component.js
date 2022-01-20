06ad0c6856c3200cd4ed809f9cda4df1e4a54e8d
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2022-01-20 16:09:30 +0100

    Add mui

---
+++ src/components/header/component.js
@@ -0,0 +1,26 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import knowledgeLogo from "/img/knowledge.svg";
+const Header = () => {
+  return (
+    <header className="App-header">
+      ©
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+          >
+            <img src={knowledgeLogo} alt="knowledge logo" />
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </header>
+  );
+};
+
+export default Header;
