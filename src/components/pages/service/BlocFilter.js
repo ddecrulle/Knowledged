7b90e24c23d346f6cc2332fdb29d7d8a6da8b2fb
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2022-02-04 16:48:39 +0100

    Update BlocFilter.js

--- src/components/pages/service/BlocFilter.js
+++ src/components/pages/service/BlocFilter.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 // import { useSearchParams } from "react-router-dom";
 import Card from "@mui/material/Card";
 import { makeStyles } from "tss-react/mui";
@@ -18,51 +18,49 @@ import {
   utilisateurs,
 } from "utils/mockData/treeData";
 
-const useStyles = makeStyles()((theme) => (
-  {
-    box: {
-      display: "flex",
-      flexDirection: "column",
-      height: "100%",
-      minWidth: 50,
-    },
-    card: {
-      boxShadow: theme.shadows[4],
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      [theme.breakpoints.up("xs")]: {
-        paddingTop: theme.spacing(1),
-        paddingBottom: theme.spacing(1),
-        paddingLeft: theme.spacing(1),
-        paddingRight: theme.spacing(1),
-      },
-      [theme.breakpoints.up("sm")]: {
-        paddingTop: theme.spacing(2),
-        paddingBottom: theme.spacing(2),
-        paddingLeft: theme.spacing(2),
-        paddingRight: theme.spacing(2),
-      },
-      [theme.breakpoints.up("md")]: {
-        paddingTop: theme.spacing(3),
-        paddingBottom: theme.spacing(3),
-        paddingLeft: theme.spacing(3),
-        paddingRight: theme.spacing(3),
-      },
-      [theme.breakpoints.up("lg")]: {
-        paddingTop: theme.spacing(4),
-        paddingBottom: theme.spacing(4),
-        paddingLeft: theme.spacing(4),
-        paddingRight: theme.spacing(4),
-      },
-      maxWidth: 1280,
-      width: "100%",
-    },
-    autocomplete: {
+const useStyles = makeStyles()((theme) => ({
+  box: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+    minWidth: 50,
+  },
+  card: {
+    boxShadow: theme.shadows[4],
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    [theme.breakpoints.up("xs")]: {
+      paddingTop: theme.spacing(1),
       paddingBottom: theme.spacing(1),
-      minWidth: theme.spacing(10),
+      paddingLeft: theme.spacing(1),
+      paddingRight: theme.spacing(1),
     },
-  }
-));
+    [theme.breakpoints.up("sm")]: {
+      paddingTop: theme.spacing(2),
+      paddingBottom: theme.spacing(2),
+      paddingLeft: theme.spacing(2),
+      paddingRight: theme.spacing(2),
+    },
+    [theme.breakpoints.up("md")]: {
+      paddingTop: theme.spacing(3),
+      paddingBottom: theme.spacing(3),
+      paddingLeft: theme.spacing(3),
+      paddingRight: theme.spacing(3),
+    },
+    [theme.breakpoints.up("lg")]: {
+      paddingTop: theme.spacing(4),
+      paddingBottom: theme.spacing(4),
+      paddingLeft: theme.spacing(4),
+      paddingRight: theme.spacing(4),
+    },
+    maxWidth: 1280,
+    width: "100%",
+  },
+  autocomplete: {
+    paddingBottom: theme.spacing(1),
+    minWidth: theme.spacing(10),
+  },
+}));
 
 const buildNode = (obj, key) => {
   if (!obj[key]) {
@@ -118,7 +116,6 @@ const BlocFilter = () => {
           {buildNode(services, services.nodeKey)}
           {buildNode(statuts, statuts.nodeKey)}
           {buildNode(utilisateurs, utilisateurs.nodeKey)}
-          
         </TreeView>
       </Card>
     </Box>
