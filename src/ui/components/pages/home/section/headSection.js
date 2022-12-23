b26664da6eafcdff5f4011bcc7b5a6f519147962
Dylan Decrulle <dylan.decrulle@outlook.fr>
2022-12-23 15:35:45 +0100

    feat: remove unused asset and chose head picture

--- src/ui/components/pages/home/section/headSection.js
+++ src/ui/components/pages/home/section/headSection.js
@@ -6,7 +6,7 @@ import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import Hidden from '@mui/material/Hidden';
 import Box from '@mui/system/Box';
-import picture from 'ui/components/assets/img/funny-portrait-of-a-businessman-with-many-arms-picture.jpg';
+import picture from 'ui/components/assets/img/babel_tower.png';
 import { Link } from 'react-router-dom';
 import homePage from 'ui/components/constants/page/homePage';
 
