3b6f2fd529da64a9d2f9e765c531b3e111ca2e2b
Dylan Decrulle <dylan.decrulle@outlook.fr>
2023-01-06 23:05:08 +0100

    feat: add chip

--- src/ui/components/pages/service/section/functions/function.js
+++ src/ui/components/pages/service/section/functions/function.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import { makeStyles } from 'tss-react/mui';
+import { Chip } from '@mui/material';
 
 export const Function = ({ fct }) => {
 	const { classes } = useStyles();
-
 	return (
-		<li>
+		<li className={classes.li}>
 			<Typography
 				variant='subtitle1'
 				textTransform='uppercase'
@@ -14,13 +14,34 @@ export const Function = ({ fct }) => {
 			>
 				{fct['label']}
 			</Typography>
-			<Typography variant='body1' color='GrayText'>
+			<Typography
+				variant='body1'
+				color='GrayText'
+				className={classes.descriptionText}
+			>
 				{fct['description']}
 			</Typography>
+			{fct.products.map((p) => (
+				<Chip
+					key={p.id}
+					className={classes.chip}
+					label={p.label}
+					color='secondary'
+				/>
+			))}
 		</li>
 	);
 };
 
-const useStyles = makeStyles()(() => ({
-	fctLabel: {},
+const useStyles = makeStyles()((theme) => ({
+	li: {
+		marginBottom: theme.spacing(1),
+	},
+	descriptionText: {
+		//marginBottom: theme.spacing(1),
+	},
+	chip: {
+		marginTop: theme.spacing(1),
+		marginRight: theme.spacing(0.5),
+	},
 }));
