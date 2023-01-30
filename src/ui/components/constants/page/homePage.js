e3bbd0b8be71f4600f89b256132d3da08fda14cf
Dylan Decrulle <dylan.decrulle@outlook.fr>
2023-01-30 15:00:36 +0100

    ci: add poke gitops

--- src/ui/components/constants/page/homePage.js
+++ src/ui/components/constants/page/homePage.js
@@ -2,7 +2,7 @@ const homePage = {
 	headSection: {
 		title: 'Knowledge',
 		text: 'Bibliothèque de connaissances de l’offre de services du système d’information de collecte de enquêtes ménages et entreprises de l’Insee',
-		buttonServiceOffer: 'Accèder à l’offre de services',
+		buttonServiceOffer: 'Visite de la bibliothèque',
 	},
 };
 export default homePage;
