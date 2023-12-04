a5b63419e79f97f3fb90e22a8289bb2dd2301f28
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2023-12-04 09:31:57 +0100

    build: dev dep and move to esm build

--- scripts/generate-entrypoint.js
+++ scripts/generate-entrypoint.js
@@ -1,23 +1,25 @@
 /* eslint-disable */
-var fs = require('fs');
+import { promises as fs } from 'fs';
 
-fs.readFile('.env', 'utf8', function (_, contents) {
-  const content = contents
-    .split('\n')
-    .filter((line) => !line.startsWith('#'))
-    .map((line) => line.split('='))
-    .filter((data) => data.length === 2)
-    .map(
-      ([key]) =>
-        `echo "window._env_['${key.replace(
-          'REACT_APP_',
-          ''
-        )}'] = '\$${key.replace(
-          'REACT_APP_',
-          ''
-        )}';" >> /usr/share/nginx/html/env-config.js`
-    );
+fs.readFile('.env', 'utf8')
+  .then((contents) => {
+    const content = contents
+      .split('\n')
+      .filter((line) => !line.startsWith('#'))
+      .map((line) => line.split('='))
+      .filter((data) => data.length === 2)
+      .map(
+        ([key]) =>
+          `echo "window._env_['${key.replace(
+            'REACT_APP_',
+            ''
+          )}'] = '\$${key.replace(
+            'REACT_APP_',
+            ''
+          )}';" >> /usr/share/nginx/html/env-config.js`
+      );
 
-  const fullFile = ['#!/bin/sh', ...content, 'exec "$@"'].join('\n');
-  fs.writeFileSync('entrypoint.sh', fullFile, 'utf8');
-});
+    const fullFile = ['#!/bin/sh', ...content, 'exec "$@"'].join('\n');
+    return fs.writeFile('entrypoint.sh', fullFile, 'utf8');
+  })
+  .catch((error) => console.error('Error:', error));
